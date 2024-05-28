2e50e8bcccf2bfaf9b4d7afb6f29baff61e027d9
rakesh1423 <rakeshkumarpatra1423@gmail.com>
2024-05-29 00:45:00 +0530

    Integrated the frontend with the backend

--- base/urls.py
+++ base/urls.py
@@ -3,14 +3,18 @@ from . import views
 
 urlpatterns=[
     path('login/', views.loginPage, name="login"),
+    path('login.html', views.loginPage),  
     path('logout/', views.logoutUser, name="logout"),
     path('register/', views.registerPage, name="register"),
     path('', views.home,name="home"),
     path('room/<str:pk>/', views.room, name="room"),
     path('profile/<str:pk>', views.userProfile, name='user-profile'),
-    path('create-room/', views.createRoom, name="create-room"),
+    path('create-room.html', views.createRoom, name="create-room"),
     path('update-room/<str:pk>/', views.updateRoom, name="update-room"),
     path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room"),
     path('delete-message/<str:pk>/', views.deleteMessage, name="delete-message"),
+    path('update-user/', views.updateUser, name="update-user"),
+    path('update-user/<str:pk>/', views.updateUser, name="update-user-detail"),
+    path('topics/', views.topicsPage, name="topics"),
+    path('activity/', views.activityPage, name="activity"),
 ]
-
